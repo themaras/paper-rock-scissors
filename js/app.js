c38b88470d1a1d15cd88aa06a69afe28bf4b39a1
Themistoklis Papaioannou <themaras3@gmail.com>
2015-02-10 01:37:30 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,52 +1,58 @@
 $(document).ready(function () {
 
     //User selects "paper"
-    $('.paperB').mousedown(function () {
+    $('#paperB').one("click", function () {
         $('.paperP').show();
         compChoice();
         $('.makeChoice').hide();
-        if(computerChoice==="rock"){
-        	setFeedback("You win!")
-        }else if(computerChoice==="scissors"){
-        	setFeedback("Computer wins!")
-        }else {
-        	setFeedback("It's a tie!")
+        if (computerChoice === "rock") {
+            setFeedback("You win!");
+        } else if (computerChoice === "scissors") {
+            setFeedback("Computer wins!");
+        } else {
+            setFeedback("It's a tie!");
         }
+        $('#rockB').hide();
+        $('#scissorsB').hide();
     });
 
     //User selects "rock"
-    $('.rockB').mousedown(function () {
+    $('#rockB').one("click", function () {
         $('.rockP').show();
         compChoice();
         $('.makeChoice').hide();
-        if(computerChoice==="scissors"){
-        	setFeedback("You win!")
-        }else if(computerChoice==="paper"){
-        	setFeedback("Computer wins!")
-        }else {
-        	setFeedback("It's a tie!")
+        if (computerChoice === "scissors") {
+            setFeedback("You win!");
+        } else if (computerChoice === "paper") {
+            setFeedback("Computer wins!");
+        } else {
+            setFeedback("It's a tie!");
         }
+        $('#paperB').hide();
+        $('#scissorsB').hide();
     });
 
     //User selects "scissors"
-    $('.scissorsB').mousedown(function () {
+    $('#scissorsB').one("click", function () {
         $('.scissorsP').show();
         compChoice();
         $('.makeChoice').hide();
-        if(computerChoice==="paper"){
-        	setFeedback("You win!")
-        }else if(computerChoice==="rock"){
-        	setFeedback("Computer wins!")
-        }else {
-        	setFeedback("It's a tie!")
+        if (computerChoice === "paper") {
+            setFeedback("You win!");
+        } else if (computerChoice === "rock") {
+            setFeedback("Computer wins!");
+        } else {
+            setFeedback("It's a tie!");
         }
+        $('#paperB').hide();
+        $('#rockB').hide();
     });
 
+    //Generate computer option
+    var computerChoice = Math.random();
 
-	var computerChoice = Math.random();
-
-        function compChoice() {
-         if (computerChoice < 0.34) {
+    function compChoice() {
+        if (computerChoice < 0.34) {
             computerChoice = "rock";
             $('.rockC').show();
         } else if (computerChoice <= 0.67) {
@@ -59,11 +65,8 @@ $(document).ready(function () {
         console.log("Computer: " + computerChoice);
     }
 
-
-        /*--- Set the feedback ---*/
+    /*--- Set the feedback ---*/
     function setFeedback(feedback) {
         $('.feedback').text(feedback);
     }
-
-
 });
